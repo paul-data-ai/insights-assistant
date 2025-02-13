6c250a478448b9a07385ef75c36963b6fe152dac
paul <paullkingss@gmail.com>
2025-02-13 17:53:09 +0000

    modified Slack secrets

--- main.py
+++ main.py
@@ -71,14 +71,15 @@ def fetch_insights():
 
     return insights, images, dataframes
 
+
+SLACK_WEBHOOK_URL = os.getenv('SLACK_WEBHOOK')
+SLACK_BOT_TOKEN = os.getenv('SLACK_BOT_TOKEN')
+
+
 # Send insights, images, and CSV files to Slack
 def send_to_slack(insights, images, dataframes):
-    SLACK_WEBHOOK_URL = os.getenv('SLACK_WEBHOOK')
     print(f"SLACK_WEBHOOK_URL: {'SET' if SLACK_WEBHOOK_URL else 'MISSING'}")
-
-    SLACK_BOT_TOKEN = os.getenv('SLACK_PASS')
     print(f"SLACK_BOT_TOKEN: {'SET' if SLACK_BOT_TOKEN else 'MISSING'}")
-
 
     SLACK_CHANNEL = "#insights"
 
